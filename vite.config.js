0d00ee341d9027d2b728f6e2b14747232ddcb601
Steinmayer <grutno228@gmail.com>
2025-12-15 20:16:21 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Относительные пути для корректной работы в любой папке
-})
